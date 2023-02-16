a669a0745c33d1314ffe73810fd9067b8b6a656b
ctnkimty <94091447+ctnkimty@users.noreply.github.com>
2023-02-16 14:40:02 +0900

    Update HttpUtilTest.java

--- src/test/java/com/kollus/kr/kollus_sample_java/test/HttpUtilTest.java
+++ src/test/java/com/kollus/kr/kollus_sample_java/test/HttpUtilTest.java
@@ -10,7 +10,7 @@ public class HttpUtilTest {
 	public static void main(String[] args) {
 
 		HttpUtil httpUtil = new HttpUtil();
-		String _url = "http://api.kr.kollus.com/0/media/category/index?access_token=7ge80tfvz51x2606";
+		String _url = "http://api.kr.kollus.com/0/media/category/index?access_token=API접근토큰";
 		HashMap<String, String> headers = new HashMap<String, String>();
 		headers.put("Accept", "application/xml; charset=utf-8");
 		headers.put("Content-Type", "application/xml; charset=utf-8");
